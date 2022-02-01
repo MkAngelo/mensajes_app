44f78aee03783997e5063a4ddff4caecbb549d3b
MkAngelo <mikequi000@gmail.com>
2022-02-01 16:34:33 -0600

    Mensajes App

---
+++ src/main/java/com/mycompany/mensajes_app/inicio.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.mensajes_app;
+
+/**
+ *
+ * @author mkangelo
+ */
+public class inicio {
+    public static void main() {
+        
+    }
+}
